1adace272c62c7d68d26ea37b6521f37a10e8980
Andrew Horn <arhorn@smcm.edu>
2020-04-07 11:20:25 -0500

    now functions correctly

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,55 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./style.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <div className="container">
+      <h2>API Project: URL Shortener Microservice</h2>
+      <h3>User Story: </h3>
+      <ol>
+        <li>
+          I can POST a URL to <code>[project_url]/api/shorturl/new</code> and I
+          will receive a shortened URL in the JSON response.
+          <br />
+          Example :{" "}
+          <code>{'{"original_url":"www.google.com","short_url":1}'}</code>
+        </li>
+        <li>
+          If I pass an invalid URL that doesn't follow the{" "}
+          <code>http(s)://www.example.com(/more/routes)</code> format, the JSON
+          response will contain an error like{" "}
+          <code>{'{"error":"invalid URL"}'}</code>
+        </li>
+        <li>
+          When I visit the shortened URL, it will redirect me to my original
+          link.
+        </li>
+      </ol>
+
+      <h3>Short URL Creation </h3>
+      <p>
+        example: <code>POST https://flask-url-shortener.andrew-horn-portfolio.life/api/shorturl/new</code> -{" "}
+        <code>https://www.google.com</code>
+      </p>
+      <form action="https://flask-url-shortener.andrew-horn-portfolio.life/api/shorturl/new" method="POST">
+        <label for="url_input">URL to be shortened</label>
+        <input
+          id="url_input"
+          type="text"
+          name="url"
+          value="https://www.google.com"
+        />
+        <input type="submit" value="POST URL" />
+      </form>
+      <h3>Example Usage:</h3>
+      <a href="/api/shorturl/3">https://flask-url-shortener.andrew-horn-portfolio.life/api/shorturl/3</a>
+
+      <h3>Will Redirect to:</h3>
+      <p>https://www.washingtonpost.com</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
